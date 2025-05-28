eb7b0173f4575f18b62e0f20c358ade9e6cf5247
Your Name <marc@w4ovt.org>
2025-05-28 10:29:26 -0400

    Save changes before removing lowercase activationcount.tsx

--- src/app/activationcount.tsx
+++ src/app/activationcount.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-export default function activationcount() {
+export default function ActivationCount() {
   const [count, setCount] = useState<number | null>(null);
 
   useEffect(() => {
