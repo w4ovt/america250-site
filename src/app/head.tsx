fc2d7fa5eaf842bed851a124aa3a1aaeb3ebe2a6
Your Name <marc@w4ovt.org>
2025-06-04 10:08:51 -0400

    toolbar redesign

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+// src/app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      {/* Add other meta tags and <title> as needed */}
+    </>
+  );
+}
+// This file is used to define the head section of the HTML document in a Next.js application.
